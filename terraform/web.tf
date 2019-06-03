3f11a41a4e4c56c5025698b0cfc545d0b6031d5c
pksebben <benmorsillo@gmail.com>
2019-06-03 17:33:48 -0600

    modified web.tf terra init terra plan

--- terraform/web.tf
+++ terraform/web.tf
@@ -11,8 +11,6 @@ resource "google_compute_instance" "vm_instance" {
   network_interface {
     # A default network is created for all GCP projects
     network = "${google_compute_network.vpc_network.self_link}"
-    access_config = {
-    }
   }
 
   tags = [
