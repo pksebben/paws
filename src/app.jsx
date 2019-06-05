47d995130e756007654e31046996221335836ce3
pksebben <benmorsillo@gmail.com>
2019-06-05 14:03:48 -0600

    Cleaned up temp files.

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,15 @@
 //TODO: Implement routing for gamerprofile and shelterprofile
 //TODO: Git this whole project and send to kirby
 //KIRBY: Hi!
+//this is a test of the automated leave-fucking-files-everywhere system.
+//this is another test of the alffes.
+//c'mon, save already you stupid piece of..
+//JUST DO IT!
+//ATTAIN YOUR GOALS!
+//RUNNING THROUGH THE TREES, SHIA LEBOUF!
+//BITING AT YOUR KNEES, IT'S SHIA LEBOUF!
+//CREEPING IN THE SHAAAAAADOWWWSSSSS...
+//...HOLLYWOOD SUPERSTAR, SHIA LEBOUF!
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { Component,Suspense,lazy } from 'react';
