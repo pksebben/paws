cbafa1b970bd517025a44ffd9878d17bc2dc1222
pksebben <benmorsillo@gmail.com>
2019-08-27 12:56:25 -0600

    minor styling change

--- python/pyg/web/api.py
+++ python/pyg/web/api.py
@@ -1,6 +1,7 @@
 import flask
 import werkzeug.exceptions
 from .models import Person, UserAuth, UserProfile, Org, OrgMembership, Fundraiser
+from sqlwizardry import SQLAlchemy
 
 bp = flask.Blueprint('api', __name__, url_prefix='/api/v1')
 
