c14af6853bdefe52c40f2a90f2fc5507c337379f
Ian N Sung-Schenck <ian.schenck@gmail.com>
2019-12-12 15:57:55 -0700

    Add admin and tweak models.

--- python/pyg/web/models.py
+++ python/pyg/web/models.py
@@ -28,12 +28,7 @@ class Member(Base):
 
     id = Column(Integer, primary_key=True)
     created = Column(DateTime, nullable=False)
-    auth = relationship(
-        "Auth",
-        backref=backref(
-            "member",
-            uselist=False),
-        uselist=False)
+    auth = relationship("Auth", uselist=False)
     profile = relationship(
         "Profile",
         backref=backref(
@@ -45,6 +40,9 @@ class Member(Base):
         back_populates="member"
     )
     teams = relationship("Team", secondary=member_to_team)
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Auth(Base):
@@ -59,6 +57,10 @@ class Auth(Base):
     name = Column(String(80), unique=False, nullable=False)
     password = Column(String(80), unique=False, nullable=False)
     email = Column(String(80), unique=True, nullable=False)
+    member = relationship("Member", uselist=False)
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Profile(Base):
@@ -77,11 +79,15 @@ class Profile(Base):
     location = Column(String(40))
     twitch_handle = Column(String(40))
 
+    def __str__(self):
+        return self.handle
+
 
 class Team(Base):
     __tablename__ = 'team'
 
     id = Column(Integer, primary_key=True)
+    name = Column(String(80))
     date_joined = Column(Date)
     missionstatement = Column(Text, nullable=True)
     location = Column(String(50), nullable=True, unique=False)
@@ -91,6 +97,9 @@ class Team(Base):
     twitch_url = Column(Text, nullable=True)
     instagram_url = Column(Text, nullable=True)
     members = relationship("Member", secondary=member_to_team)
+
+    def __str__(self):
+        return self.name
 
 
 class Donation(Base):
@@ -102,6 +111,9 @@ class Donation(Base):
     created = Column(DateTime, nullable=False)
     member_id = Column(Integer, ForeignKey("member.id"))
     member = relationship("Member", back_populates="donations")
+
+    def __str__(self):
+        return "$%.2f" % self.amount
 
 
 class NewsArticle(Base):
@@ -115,11 +127,17 @@ class NewsArticle(Base):
     snippet = Column(Text(convert_unicode=True, length=250), nullable=False)
     body = Column(Text(convert_unicode=True))
 
+    def __str__(self):
+        return self.headline
+
 
 class Route(Base):
     __tablename__ = "route"
     id = Column(String(80), primary_key=True)
     texts = relationship("Text", back_populates="route")
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Text(Base):
