da8cc1df47ee8a5fb73d3fcfe5ee52933c24c403
pksebben <benmorsillo@gmail.com>
2019-12-12 13:38:52 -0700

    modularized leaderboard implemented leaderboard-in-homepage

--- python/pyg/web/views/home.py
+++ python/pyg/web/views/home.py
@@ -1,6 +1,6 @@
 import flask
 from flask import render_template
-from sqlalchemy import desc
+from sqlalchemy import desc, func
 
 from pyg.web import models, db
 
@@ -9,6 +9,14 @@ bp = flask.Blueprint('home', __name__)
 
 @bp.route('/')
 def home():
+    donations = db.web.session.query(
+        models.Profile.handle,
+        func.sum(models.Donation.amount).label('total')
+    ).join(models.Member.profile
+    ).join(models.Donation
+    ).group_by(
+        models.Profile.handle).order_by(desc('total')).all()
+    leaderboard_players = enumerate(donations, start=1)
     news = db.web.session.query(models.NewsArticle).order_by(
         desc("datetime"))
-    return render_template('content_home.html', news=news)
+    return render_template('content_home.html', news=news, leaderboard_players=leaderboard_players)
