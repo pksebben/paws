7bb7a371d39b1ddb3080fdfc1531ec9d8966c452
Kirby <mshhll.rk@gmail.com>
2020-02-04 16:22:30 -0800

    Synced search styling across nav and search page

--- python/pyg/web/static/js/scripts.js
+++ python/pyg/web/static/js/scripts.js
@@ -2,17 +2,13 @@ jQuery(document).ready(function( $ ) {
         $nav_news = $('.nav-news');
         $('.nav-search-icon').on('click', function(event){
                 event.preventDefault();
-                $('.nav-news').animate({width:'toggle'},1000);
-                $('.nav-our_tail').animate({width:'toggle'},1000);
-                $('.nav-search-form_container').animate({width:'toggle'},1000);
+                $('.nav-search-form_container').slideToggle();
                 $('.nav-search').toggleClass('active');
         });
 
         $('.nav-search-exit').on('click', function(event){
                 event.preventDefault();
-                $('.nav-news').show();
-                $('.nav-our_tail').show();
-                $('.nav-search-form_container').animate({width:'toggle'},350);
+                $('.nav-search-form_container').slideToggle();
                 $('.nav-search').toggleClass('active');
         });
 });
