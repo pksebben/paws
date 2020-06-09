dfba4ff191cbba3e75dcb27ab45ce2b00a44accc
pksebben <benmorsillo@gmail.com>
2020-06-09 12:06:11 -0700

    added filetype validation for avatar upload

--- python/pyg/web/views/avatar_upload.py
+++ python/pyg/web/views/avatar_upload.py
@@ -2,6 +2,7 @@ import os
 import datetime
 
 import flask
+from wtforms import Form, FileField, validators, ValidationError
 
 from pyg.web import db, models
 
@@ -16,11 +17,21 @@ def allowed_file(filename):
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def invalid_filetype(form, field):
+    """if this ends up working, leave the rest of this docstring
+this validator always fails.  It is meant to be called IOT cause wtforms to properly display a validation message.
+It is a (terribly hacky) workaround for the fact that wtforms does not actually wrap the fileupload field around the file data to be uploaded.  Why they would even bother having such a field, IDK.  maybe it works in places other than flask.  Shit design, IMHO.
+ """
+    print("filetype invalidated")
+    raise ValidationError(
+        "Invalid filetype.  Please upload a jpg, png, or gif.")
+
+
 """
 Avatar Upload
-Members may upload avatars in {what formats?}.  The file will live in /static/avatars/ 
+Members may upload avatars in {what formats?}.  The file will live in /static/avatars/
 
-Avatar image names are automatically applied as the image uploads (In order to circumvent directory traversal attacks), and take the format { 'av_' + timestamp + '_' + member id }.  This name is saved in the 'avatar url' field in their profile table. 
+Avatar image names are automatically applied as the image uploads (In order to circumvent directory traversal attacks), and take the format { 'av_' + timestamp + '_' + member id }.  This name is saved in the 'avatar url' field in their profile table.
 
 This route will eventually become an api call and reroute to the member profile page.
 TODO(ben): Validation errors ("wrong filetype, meathead etc.")
@@ -28,21 +39,25 @@ TODO(ben): Turn this into an API call / implement in member profile
 TODO(ben): avatar filetype checking
 
 """
+
+
+class UploadForm(Form):
+    avatar = FileField(validators=[])
+
+
 @bp.route('/avatar_upload', methods=['POST', 'GET'])
 def upload_file():
-    if flask.request.method == 'POST':
-        print(dir(flask.request.files))
-        print(flask.request.files)
-        
+    avatarform = UploadForm(flask.request.form)
+    if flask.request.method == 'POST' and avatarform.validate():
         if 'avatar' not in flask.request.files:
-            print('no file part')
+            print('no file uploaded')
             return flask.redirect(flask.request.url)
         file = flask.request.files['avatar']
         if file.filename == '':
             print('no selected file')
             return flask.redirect(flask.request.url)
-        if file and allowed_file(file.filename):
-            # place a pointer in the db and make it the filename
+        if file.filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
+            # do uploady stuff
             member = db.web.session.query(
                 models.Member).get(
                 flask.session['userid'])
@@ -55,5 +70,11 @@ def upload_file():
             member.avatar_url = str(filename)
             db.web.session.commit()
             print('saved!!')
-
-    return flask.render_template("upload_avatar.html")
+            gotostr = "/profile/" + str(flask.session['userid'])
+            return flask.redirect(gotostr)
+        else:
+            avatarform.avatar.validate(avatarform, extra_validators=[invalid_filetype])
+            return flask.render_template("upload_avatar.html", avatarform=avatarform)
+            # place a pointer in the db and make it the filename
+    return flask.render_template(
+        "upload_avatar.html", avatarform=avatarform)
